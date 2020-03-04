cb3d9eea3daad343ded7a063e51e0aaac7537eac
Boopya <rikiyaeduarteyamamoto@gmail.com>
2020-03-04 21:11:23 +0800

    initial commit

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class LoginFrame {
     JFrame frame;
     JPanel pane;
-    JLabel usernameLabel, passwordLabel;
+    JLabel usernameLabel, passwordLabel, titleLabel;
     JTextField usernameField;
     JPasswordField passwordField;
     JButton loginButton;
@@ -22,55 +22,69 @@ public class LoginFrame {
 
     public void initComponents() {
         frame = new JFrame();
-        frame.setTitle("Password Manager");
-        frame.setVisible(true);
+        frame.setTitle("Login");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
         pane = new JPanel(new GridBagLayout());
         frame.add(pane);
 
         GridBagConstraints c = new GridBagConstraints();
-
+        
+        titleLabel = new JLabel("Password Manager", SwingConstants.CENTER);
         c.insets = new Insets(15,15,15,15);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.ipady = 20;
-        c.weighty = 0.5;
-
-        usernameLabel = new JLabel("Username: ", SwingConstants.CENTER);
         c.gridx = 0;
         c.gridy = 0;
-        c.ipadx = 100;
+        c.gridwidth = 2;
+        pane.add(titleLabel, c);
+        
+        c.gridwidth = 1;
+        
+        usernameLabel = new JLabel("Username: ", SwingConstants.CENTER);
+        c.insets = new Insets(15,15,15,0);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.ipadx = 50;
+        c.ipady = 20;
         c.weightx = 0.3;
         pane.add(usernameLabel, c);
 
         passwordLabel = new JLabel("Password: ", SwingConstants.CENTER);
+        c.insets = new Insets(15,15,15,0);
         c.gridx = 0;
-        c.gridy = 1;
-        c.ipadx = 100;
+        c.gridy = 2;
+        c.ipadx = 50;
+        c.ipady = 20;
         c.weightx = 0.3;
         pane.add(passwordLabel, c);
-
+        
         usernameField = new JTextField();
+        c.insets = new Insets(15,0,15,15);
         c.gridx = 1;
-        c.gridy = 0;
-        c.ipadx = 200;
+        c.gridy = 1;
+        c.ipadx = 150;
+        c.ipady = 12;
         c.weightx = 0.7;
         pane.add(usernameField, c);
 
         passwordField = new JPasswordField();
+        c.insets = new Insets(15,0,15,15);
         c.gridx = 1;
-        c.gridy = 1;
-        c.ipadx = 200;
+        c.gridy = 2;
+        c.ipadx = 150;
+        c.ipady = 12;
         c.weightx = 0.7;
         pane.add(passwordField, c);
 
         loginButton = new JButton("Login");
+        c.insets = new Insets(15,15,15,15);
+        c.anchor = GridBagConstraints.PAGE_END;
+        c.ipadx = 20;
+        c.ipady = 10;
         c.gridx = 0;
-        c.gridy = 2;
-        c.fill = GridBagConstraints.NONE;
-        c.weightx = 0.5;
+        c.gridy = 3;
         c.gridwidth = 2;
         pane.add(loginButton, c);
 
